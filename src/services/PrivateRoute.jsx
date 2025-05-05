567efa3d9c4e4ec860381c1d9325bd2c8d99f8d0
vasanth <intraintech.vasanth@gmail.com>
2025-05-05 23:38:59 +0530

    updated

--- src/services/PrivateRoute.jsx
+++ src/services/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "./AuthContext";
-
+import React from "react";
 const PrivateRoute = ({ children }) => {
   const { auth } = useContext(AuthContext);
 
